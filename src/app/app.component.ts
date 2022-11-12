1add54652dbf0042bce9ad04447cbe95253d6c13
Joni <joni.786@hotmail.co.uk>
2022-11-12 16:45:14 +0000

    draft angular test project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-ng-app';
+  // listenNumber = 0;
+
+  // onListening(numbers: number): void {
+  //   this.listenNumber = numbers;
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './main/shopping-app/Header/header/header.component';
+import { ShoppingListComponent } from './main/shopping-app/Shopping/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './main/shopping-app/Shopping/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './main/shopping-app/RecipeBook/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './main/shopping-app/RecipeBook/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './main/shopping-app/RecipeBook/recipe-detail/recipe-detail.component';
+import { GameControlComponent } from './main/even-odd-app/game-control/game-control.component';
+import { OddComponent } from './main/even-odd-app/odd/odd.component';
+import { EvenComponent } from './main/even-odd-app/even/even.component';
+import { ActiveUserComponent } from './main/server-app/active-user/active-user.component';
+import { InactiveUserComponent } from './main/server-app/inactive-user/inactive-user.component';
+import { CreateUserComponent } from './main/server-app/create-user/create-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    GameControlComponent,
+    OddComponent,
+    EvenComponent,
+    ActiveUserComponent,
+    InactiveUserComponent,
+    CreateUserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
